b4ee31619c55c275101ae80f6bc701126d1b4cb0
conegamer1 <conegamer1@gmail.com>
2026-03-10 16:44:42 +0700

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,34 @@
+const PROJECTS = [
+
+{
+name:"IOPN Testnet",
+tasks:[
+"Claim faucet",
+"Bridge tokens",
+"Swap tokens",
+"Mint NFT",
+"Interact with smart contract"
+]
+},
+
+{
+name:"Monad Testnet",
+tasks:[
+"Connect wallet",
+"Claim faucet",
+"Mint NFT",
+"Swap token"
+]
+},
+
+{
+name:"Fuel Network",
+tasks:[
+"Join discord",
+"Claim faucet",
+"Deploy contract",
+"Send transaction"
+]
+}
+
+]
